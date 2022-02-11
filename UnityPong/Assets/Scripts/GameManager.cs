a5c2ed94abe1af90578e65391cd0e6e231e76e02
Erkberg <erisch01@web.de>
2022-02-11 12:34:38 +0100

    animated score UI, tutorial texts

--- UnityPong/Assets/Scripts/GameManager.cs
+++ UnityPong/Assets/Scripts/GameManager.cs
@@ -33,11 +33,20 @@ public class GameManager : MonoBehaviour
             scorePlayer2++;
 
         UpdateScores();
+        HighlightScore(id);
     }
 
     private void UpdateScores()
     {
         scoreTextLeft.SetScore(scorePlayer1);
         scoreTextRight.SetScore(scorePlayer2);
+    }
+
+    public void HighlightScore(int id)
+    {
+        if (id == 1)
+            scoreTextLeft.Highlight();
+        else
+            scoreTextRight.Highlight();
     }
 }
--- UnityPong/Assets/Scripts/ScoreText.cs
+++ UnityPong/Assets/Scripts/ScoreText.cs
@@ -6,6 +6,12 @@ using TMPro;
 public class ScoreText : MonoBehaviour
 {
     public TextMeshProUGUI text;
+    public Animator animator;
+
+    public void Highlight()
+    {
+        animator.SetTrigger("highlight");
+    }
 
     public void SetScore(int value)
     {
